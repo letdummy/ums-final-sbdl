510cab67edb7c19d46e6a0fe0e56db7a8d4ca7ce
hussainabdillah <hussainkelarno@gmail.com>
2023-06-23 13:04:24 +0700

    update login menu

--- main.py
+++ main.py
@@ -9,34 +9,23 @@ def login_menu():
     print("2. Login as Admin")
     print("3. Exit")
 
-def creation_menu():
-    print("\n========================== Linked List Creation Menu =========================\n")
-    print("1. Create a Book")
-    print("2. Display a Book")
+def login_member_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Borrow a Book")
+    print("2. Return a Book")
     print("3. Display Borrowed Book")
-    print("4. Display Linked List")
-    print("5. Reverse Linked List")
-    print("6. Palindrome Check")
-    print("7. Back to main menu (delete current linkedlist)")
-    print("8. Exit")
 
-
-def edit_menu():
-    print("\n================================== Edit Node =================================\n")
-    print("1. Insert Node at the beginning")
-    print("2. Delete Node at the beginning")
-    print("3. Insert Node at the end")
-    print("4. Delete Node at the end")
-    print("5. Back to creation menu")
-    print("6. Exit")
-
-
-def indexed_edit_menu():
-    print("\n============================ Edit Node by Position ===========================\n")
-    print("1. Insert Node at certain position")
-    print("2. Delete Node at certain position")
-    print("3. Back to creation menu")
+def login_admin_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Add a Book")
+    print("2. Remove a Book")
+    print("3. Display Book List")
     print("4. Exit")
 
 
-print("Linked List Successfully Created!")
+
+
+
+
+
+
