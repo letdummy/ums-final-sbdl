b67dfe55a0874274da3eca8c2a193e9465aa20a6
hussainabdillah <hussainkelarno@gmail.com>
2023-06-23 13:19:24 +0700

    add login feature

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+# mainMenu
 def main_menu():
     print("\n================================= Library UMS ================================\n")
     print("1. Login")
     print("2. Exit")
 
+# loginMenu
 def login_menu():
     print("\n================================= Library UMS ================================\n")
     print("1. Login as Member")
     print("2. Login as Admin")
     print("3. Exit")
 
+# member & admin menu
 def login_member_menu():
     print("\n================================= Library UMS ================================\n")
     print("1. Borrow a Book")
     print("2. Return a Book")
     print("3. Display Borrowed Book")
+    print("4. Exit")
 
 def login_admin_menu():
     print("\n================================= Library UMS ================================\n")
@@ -21,6 +25,47 @@ def login_admin_menu():
     print("2. Remove a Book")
     print("3. Display Book List")
     print("4. Exit")
+
+# member
+def borrow_book_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Borrow a Book")
+    print("2. Back to Member Menu")
+    print("3. Exit")
+
+def return_book_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Return a Book")
+    print("2. Back to Member Menu")
+    print("3. Exit")
+
+def display_borrowed_book_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Display Borrowed Book")
+    print("2. Back to Member Menu")
+    print("3. Exit")
+
+# admin
+def add_book_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Add a Book")
+    print("2. Back to Admin Menu")
+    print("3. Exit")
+
+def remove_book_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Remove a Book")
+    print("2. Back to Admin Menu")
+    print("3. Exit")
+
+def display_book_list_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Display Book List")
+    print("2. Back to Admin Menu")
+    print("3. Exit")
+
+
+
 
 
 print("test changes")
