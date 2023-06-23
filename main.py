174f38dc3a98b5ef6ba8baf7903af7b29a373b04
hussainabdillah <hussainkelarno@gmail.com>
2023-06-23 12:59:12 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+def main_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Login")
+    print("2. Exit")
+
+def login_menu():
+    print("\n================================= Library UMS ================================\n")
+    print("1. Login as Member")
+    print("2. Login as Admin")
+    print("3. Exit")
+
+def creation_menu():
+    print("\n========================== Linked List Creation Menu =========================\n")
+    print("1. Create a Book")
+    print("2. Display a Book")
+    print("3. Display Borrowed Book")
+    print("4. Display Linked List")
+    print("5. Reverse Linked List")
+    print("6. Palindrome Check")
+    print("7. Back to main menu (delete current linkedlist)")
+    print("8. Exit")
+
+
+def edit_menu():
+    print("\n================================== Edit Node =================================\n")
+    print("1. Insert Node at the beginning")
+    print("2. Delete Node at the beginning")
+    print("3. Insert Node at the end")
+    print("4. Delete Node at the end")
+    print("5. Back to creation menu")
+    print("6. Exit")
+
+
+def indexed_edit_menu():
+    print("\n============================ Edit Node by Position ===========================\n")
+    print("1. Insert Node at certain position")
+    print("2. Delete Node at certain position")
+    print("3. Back to creation menu")
+    print("4. Exit")
+
+
+print("Linked List Successfully Created!")
